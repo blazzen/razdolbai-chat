6a117f58e0cf31eb2b1037052a6042b2510dde6c
vsavinova <vsavinova31@gmail.com>
2019-09-04 13:14:47 +0300

    fix

--- src/main/java/com/razdolbai/client/Client.java
+++ src/main/java/com/razdolbai/client/Client.java
@@ -1,7 +1,47 @@
 package com.razdolbai.client;
 
-public class Client{
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
     public static void main(String[] args) {
+        try {
+
+            try (
+                    final Socket socket = new Socket("localhost", 8081);
+                    final PrintWriter out = new PrintWriter(
+                            new OutputStreamWriter(
+                                    new BufferedOutputStream(socket.getOutputStream())));
+                    final BufferedReader in = new BufferedReader(
+                            new InputStreamReader(
+                                    new BufferedInputStream(socket.getInputStream())))
+            ) {
+                Proxy proxy = new Proxy(out);
+
+                while (true) {
+                    BufferedReader reader =
+                            new BufferedReader(new InputStreamReader(System.in));
+                    String input = reader.readLine();
+                    InputParser parser = new InputParser();
+                    Command command = parser.parse(input);
+                    proxy.send(command);
+
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        } catch (Exception e) {
+            // todo add logger
+            e.printStackTrace();
+        }
+
         System.out.println("Test");
+    }
+
+    private static void createNewConnection() {
+
     }
 }
