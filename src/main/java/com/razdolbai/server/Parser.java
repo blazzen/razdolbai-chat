9b4f8717506b02a1e9d6caa8e6cc9a40b4ccff86
Dmitry Lyukov <d@lyukov.com>
2019-09-04 12:40:12 +0300

    Move private methods down

--- src/main/java/com/razdolbai/server/Parser.java
+++ src/main/java/com/razdolbai/server/Parser.java
@@ -6,6 +6,32 @@ class Parser {
     private static final String FIELD_DELIMITER = "\0";
     private static final String HEAD_BODY_DELIMITER = ":";
 
+    public Command parse(String message) {
+        String[] fields = message.split(FIELD_DELIMITER);
+        String type = getType(fields[0]);
+        switch (type) {
+            case "hist": {
+                return parseHistMessage(fields);
+            }
+            case "snd": {
+                return parseSendMessage(fields);
+            }
+            default: {
+                throw new IllegalArgumentException("Unknown command type: " + type);
+            }
+        }
+    }
+
+    private SendCommand parseSendMessage(String[] fields) {
+        String messageField = fields[1];
+        String message = parseField(messageField)[1];
+        return new SendCommand(message);
+    }
+
+    private HistoryCommand parseHistMessage(String[] fields) {
+        return new HistoryCommand();
+    }
+
     private String[] parseField(String message) {
         int delimiterIndex = message.indexOf(HEAD_BODY_DELIMITER);
         if (delimiterIndex == -1) {
@@ -24,31 +50,5 @@ class Parser {
             throw new IllegalArgumentException("Type should be specified in first field");
         }
         return body;
-    }
-
-    public Command parse(String message) {
-        String[] fields = message.split(FIELD_DELIMITER);
-        String type = getType(fields[0]);
-        switch (type) {
-            case "hist": {
-                return parseHistMessage(fields);
-            }
-            case "send": {
-                return parseSendMessage(fields);
-            }
-            default: {
-                throw new IllegalArgumentException("Unknown command type: " + type);
-            }
-        }
-    }
-
-    private SendCommand parseSendMessage(String[] fields) {
-        String messageField = fields[1];
-        String message = parseField(messageField)[1];
-        return new SendCommand(message);
-    }
-
-    private HistoryCommand parseHistMessage(String[] fields) {
-        return new HistoryCommand();
     }
 }
