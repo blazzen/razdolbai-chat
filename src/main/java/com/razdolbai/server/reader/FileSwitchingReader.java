b67c81a5dd07c2df247cd55d596af05ba04bd2d2
Olga <polykova.olga98@gmail.com>
2019-09-04 16:32:28 +0300

    FileSwitcherSaver added constructor for tests

---
+++ src/main/java/com/razdolbai/server/reader/FileSwitchingReader.java
@@ -0,0 +1,20 @@
+package com.razdolbai.server.reader;
+
+import java.io.File;
+import java.util.List;
+
+public class FileSwitchingReader implements Reader{
+
+    @Override
+    public List<String> getHistory() {
+
+
+
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        //for(final File fileEntry : folder)
+    }
+}
---
+++ src/main/java/com/razdolbai/server/reader/Reader.java
@@ -0,0 +1,8 @@
+package com.razdolbai.server.reader;
+
+import java.util.List;
+
+public interface Reader {
+    List<String> getHistory();
+
+}
