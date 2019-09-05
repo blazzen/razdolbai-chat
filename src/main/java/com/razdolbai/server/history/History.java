bf0ea4a6d78e782979ad0770bb03f117d35d583d
kprlns <nikita-kapralov@mail.ru>
2019-09-05 16:31:47 +0300

    History on read-write lock

--- src/main/java/com/razdolbai/server/history/History.java
+++ src/main/java/com/razdolbai/server/history/History.java
@@ -1,12 +1,13 @@
 package com.razdolbai.server.history;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface History extends AutoCloseable {
     List<String> getHistory();
 
-    void save(String message, LocalDateTime dateTime);
+    void save(String message, LocalDateTime dateTime) throws IOException;
 
     @Override
     void close() throws Exception;
--- src/main/java/com/razdolbai/server/history/HistoryAccessObject.java
+++ src/main/java/com/razdolbai/server/history/HistoryAccessObject.java
@@ -8,30 +8,46 @@ import com.razdolbai.server.history.saver.SwitchingFileSaver;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class HistoryAccessObject implements History {
-    Saver saver;
-    Reader reader;
+    private Saver saver;
+    private Reader reader;
+    private ReadWriteLock readWriteLock;
 
     public HistoryAccessObject() throws IOException {
        saver = new SwitchingFileSaver();
        reader = new SwitchingFileReader();
+       readWriteLock = new ReentrantReadWriteLock();
     }
 
 
 
     @Override
     public List<String> getHistory() {
-        return null;
+        List<String> history = null;
+        try {
+            readWriteLock.readLock();
+            history = reader.getHistory();
+        } finally {
+            readWriteLock.readLock().unlock();
+        }
+        return history;
     }
 
     @Override
-    public void save(String message, LocalDateTime dateTime) {
-
+    public void save(String message, LocalDateTime dateTime) throws IOException {
+        try {
+            readWriteLock.writeLock().lock();
+            saver.save(message, dateTime);
+        } finally {
+            readWriteLock.writeLock().unlock();
+        }
     }
 
     @Override
     public void close() throws Exception {
-
+        saver.close();
     }
 }
