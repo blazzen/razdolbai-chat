06764435896a6c96bf133387ae9b17950d7acb17
Юлия Семченкова <jools.semchenkova@gmail.com>
2019-09-05 17:35:23 +0300

    Added 2nd console

--- src/main/java/com/razdolbai/client/OutputConsole.java
+++ src/main/java/com/razdolbai/client/OutputConsole.java
@@ -2,9 +2,16 @@ package com.razdolbai.client;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class OutputConsole {
+    private static final String EXCEPTION_MESSAGE = "Exception is thrown";
+
     public static void main(String[] args) {
+        Logger logger = Logger.getLogger("ClientLogger");
+        logger.setLevel(Level.SEVERE);
+
         try (
                 final Socket socket = new Socket("localhost", 666);
                 final PrintWriter out = new PrintWriter(
@@ -22,7 +29,7 @@ public class OutputConsole {
             }
 
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.log(Level.SEVERE, EXCEPTION_MESSAGE, e);
         }
     }
 }
