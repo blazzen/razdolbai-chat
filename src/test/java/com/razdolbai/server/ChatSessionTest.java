9b1b9e772cd6603da3fd40479bd85f5aaaac0f34
vsavinova <vsavinova31@gmail.com>
2019-09-06 00:34:37 +0300

    ChatSessionTest added

---
+++ src/test/java/com/razdolbai/server/ChatSessionTest.java
@@ -0,0 +1,49 @@
+package com.razdolbai.server;
+
+import com.razdolbai.server.commands.Command;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.internal.util.reflection.Whitebox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
+public class ChatSessionTest {
+    private ChatSession session;
+    private PrintWriter socketOut;
+    private BufferedReader socketIn;
+    private Socket socket;
+
+    @Before
+    public void setUp() {
+        socket = mock(Socket.class);
+        socketIn = mock(BufferedReader.class);
+        socketOut = mock(PrintWriter.class);
+        CommandFactory commandFactory = mock(ChatCommandFactory.class);
+        session = new ChatSession("user", socket, socketIn, socketOut, commandFactory);
+    }
+
+    @Test
+    public void shouldSendMessageToSocketOut() {
+        String message = "message";
+
+        doNothing().when(socketOut).println(message);
+        doNothing().when(socketOut).flush();
+
+        session.send(message);
+
+        verify(socketOut).println(message);
+        verify(socketOut).flush();
+    }
+
+    @Test
+    public void shouldSetClosedFlagToTrueWhenCloseInvoked() throws IOException {
+        session.close();
+        assertTrue((boolean) Whitebox.getInternalState(session, "isClosed"));
+    }
+}
