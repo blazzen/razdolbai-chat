73d8d512a27e40587f1edb562950be44a05ded10
MrVi <allesta100@gmail.com>
2019-09-06 10:55:35 +0300

    small fixes

--- src/main/java/com/razdolbai/client/ClientLogger.java
+++ src/main/java/com/razdolbai/client/ClientLogger.java
@@ -6,13 +6,19 @@ import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 public class ClientLogger {
-    static Logger createLogger() throws IOException {
+    static Logger createLogger() {
         Logger logger = Logger.getLogger("ClientLogger");
         logger.setLevel(Level.INFO);
-        FileHandler handler = new FileHandler("client.log", true);
-        SimpleFormatter simple = new SimpleFormatter();
-        handler.setFormatter(simple);
-        logger.addHandler(handler);
+        FileHandler handler = null;
+        try {
+            handler = new FileHandler("client.log", true);
+            SimpleFormatter simple = new SimpleFormatter();
+            handler.setFormatter(simple);
+            logger.addHandler(handler);
+        } catch (IOException e) {
+            e.printStackTrace();
+            logger.log(Level.SEVERE, "Exception is thrown", e);
+        }
         return logger;
     }
 }
